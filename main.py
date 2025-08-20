a72cb00e56dd559fe8fb4f547af898d79e1f2162
JavanRosario <oliveiraajavan@hotmail.com>
2025-08-19 23:43:01 -0300

    initial code

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout
+from PySide6.QtWidgets import QApplication
+from main_window import MainWindow
 import sys
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
---
+++ main_window.py
@@ -0,0 +1,14 @@
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.widget = QWidget()
+        self.window_layout = QVBoxLayout()
+        self.widget.setLayout(self.window_layout)
+
+        self.setCentralWidget(self.widget)
+        self.setWindowTitle('Calculadora')
+        
