2c95e9e6051164826a778791adf19c54e81f9f93
JavanRosario <oliveiraajavan@hotmail.com>
2025-08-20 22:42:52 -0300

    icon

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from PySide6.QtWidgets import QApplication
 from main_window import MainWindow
+from PySide6.QtGui import QIcon
+from scripts.path import ICON_DIR
+import ctypes
 import sys
 
 if __name__ == '__main__':
+    # fix icon bug on windows
+    myappid = 'mycompany.myproduct.subproduct.version'
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
     app = QApplication(sys.argv)
+    icon = QIcon(str(ICON_DIR))
+    app.setWindowIcon(icon)
     window = MainWindow()
-    window.fixed_size()
+    # window.fixed_size()
+    window.setWindowIcon(icon)
+
     window.show()
     app.exec()
