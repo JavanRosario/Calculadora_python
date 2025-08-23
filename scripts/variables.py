a48f88e6357a94c75c055c135fb2a50e53d4398b
JavanRosario <oliveiraajavan@hotmail.com>
2025-08-23 15:17:20 -0300

    dysplay scripts

---
+++ scripts/variables.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+ICON_DIR = Path(__file__).parent.parent / 'img' / '1-d0347c96.ico'
+
+
+# Sizing
+BIG_FONT = 40
+MEDIUM_FONT = 24
+SMALL_FONT = 20
+DEFAUT_MARGIN = 15
+MINIUM_WIDTH = 50
